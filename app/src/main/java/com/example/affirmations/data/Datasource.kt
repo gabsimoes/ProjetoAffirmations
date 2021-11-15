64a37ef2afa0db415c2898e62de5383b87530b20
Gabrielle Simões <gabrielle.simoes@hotmail.com>
2021-11-14 22:03:07 -0300

    Projeto alterado: imagens e frases mudadas.

--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -5,16 +5,16 @@ import com.example.affirmations.model.Affirmation
 class Datasource() {
     fun loadAffirmations(): List<Affirmation> {
         return listOf<Affirmation>(
-            Affirmation(R.string.affirmation1, R.drawable.image1),
-            Affirmation(R.string.affirmation2, R.drawable.image2),
-            Affirmation(R.string.affirmation3, R.drawable.image3),
-            Affirmation(R.string.affirmation4, R.drawable.image4),
-            Affirmation(R.string.affirmation5, R.drawable.image5),
-            Affirmation(R.string.affirmation6, R.drawable.image6),
-            Affirmation(R.string.affirmation7, R.drawable.image7),
-            Affirmation(R.string.affirmation8, R.drawable.image8),
-            Affirmation(R.string.affirmation9, R.drawable.image9),
-            Affirmation(R.string.affirmation10, R.drawable.image10)
+            Affirmation(R.string.affirmation1, R.drawable.fall_out_boy),
+            Affirmation(R.string.affirmation2, R.drawable.demi_lovato),
+            Affirmation(R.string.affirmation3, R.drawable.miley_cyrus),
+            Affirmation(R.string.affirmation4, R.drawable.motley_crue),
+            Affirmation(R.string.affirmation5, R.drawable.harry_styles),
+            Affirmation(R.string.affirmation6, R.drawable.panic_at_the_disco),
+            Affirmation(R.string.affirmation7, R.drawable.one_direction),
+            Affirmation(R.string.affirmation8, R.drawable.my_chemical_romance),
+            Affirmation(R.string.affirmation9, R.drawable.little_mix),
+            Affirmation(R.string.affirmation10, R.drawable.the_wanted)
         )
     }
 }
